15c6b9f74650d9be241dacc0c0b2276008fad1e8
Naser <nasersoftcorporation2013@gmail.com>
2019-08-04 22:26:13 +0600

    Athentication Done

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+// Connect DB
+connectDB();
 app.get("/", (req, res) => res.json({ msg: "Welcome to site" }));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 // Define Routes
 app.use("/api/users", require("./routes/users"));
